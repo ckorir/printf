119589f423561e86b74832e71991509638b319f3
ckorir <61984170+ckorir@users.noreply.github.com>
2023-03-26 09:36:06 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -56,6 +56,7 @@ int flags, int width, int precision, int size)
 	int length = BUFF_SIZE - ind - 1;
 	char padd = ' ', extra_ch = 0;
 	UNUSED(size);
+	
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 	padd = '0';
 	if (is_negative)
