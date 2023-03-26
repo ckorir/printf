285508fa020e75335e9de7fb81e25f8738e84925
ckorir <chrisruto.k@gmail.com>
2023-03-26 11:06:11 -0700

    update

--- write_handlers.c
+++ write_handlers.c
@@ -55,6 +55,7 @@ int flags, int width, int precision, int size)
 {
 	int length = BUFF_SIZE - ind - 1;
 	char padd = ' ', extra_ch = 0;
+
 	UNUSED(size);
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 	padd = '0';
