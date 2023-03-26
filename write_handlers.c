29e535d8ec0f973a4d48d7dc57b07a5bfffd3dc9
ckorir <61984170+ckorir@users.noreply.github.com>
2023-03-26 21:21:10 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -86,8 +86,7 @@ int length, char padd, char extra_c)
 {
 	int i, padd_start = 1;
 
-	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width
-	== 0)
+	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 	return (0); /* printf(".0d", 0)  no char is printed */
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 	buffer[ind] = padd = ' '; /* width is displayed with padding '' */
@@ -102,24 +101,20 @@ int length, char padd, char extra_c)
 	for (i = 1; i < width - length + 1; i++)
 	buffer[i] = padd;
 	buffer[i] = '\0';
-	if (flags & F_MINUS && padd == ' ')
-	/* Asign extra char to left of buffer */
+	if (flags & F_MINUS && padd == ' ')/* Asign extra char to left of buffer */
 	{
 	if (extra_c)
 	buffer[--ind] = extra_c;
-	return (write(1, &buffer[ind], length) + write(1,
-	&buffer[1], i - 1));
+	return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 	}
-	else if (!(flags & F_MINUS) && padd == ' ')
-	/* extra char to left of buff */
+	else if (!(flags & F_MINUS) && padd == ' ')/* extra char to left of buff */
 	{
 	if (extra_c)
 	buffer[--ind] = extra_c;
 	return (write(1, &buffer[1], i - 1) + write(1,
 	&buffer[ind], length));
 	}
-	else if (!(flags & F_MINUS) && padd == '0')
-	/* extra char to left of padd */
+	else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 	{
 	if (extra_c)
 	buffer[--padd_start] = extra_c;
